e6bda710f5fbccf8537f39b37c287cb8cc79db99
Your Name <you@example.com>
2021-11-03 20:41:46 +0200

    vrushtane na blanka (PUT)

--- sample-spring-app/src/main/java/edu/gosho/samplespringapp/DokumentBlankaZaPaketiraneNaStokaZaKlientRedove.java
+++ sample-spring-app/src/main/java/edu/gosho/samplespringapp/DokumentBlankaZaPaketiraneNaStokaZaKlientRedove.java
@@ -1,6 +1,7 @@
 package edu.gosho.samplespringapp;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,8 +10,8 @@ import java.util.Arrays;
 
 @RestController
 public class DokumentBlankaZaPaketiraneNaStokaZaKlientRedove {
-    @PostMapping
-    public ArrayList<DokumentRed> dokumentRedove(@RequestBody BlankaProverka blankaProverka) {
+    @PostMapping("/paketirane-klientska-zaiavka/proverka-i-vzimane-na-blanki")
+    public ArrayList<DokumentRed> proverkaIVzimaneNaBlanki(@RequestBody BlankaProverka blankaProverka) {
         return new ArrayList<>(
                 Arrays.asList(
                         new DokumentRed("", "01.01.0001", -1),
@@ -19,4 +20,9 @@ public class DokumentBlankaZaPaketiraneNaStokaZaKlientRedove {
                 )
         );
     }
+    @PutMapping("/paketirane-klientska-zaiavka/vryshtane-na-blanka")
+    public void vrushtaneNaBlanka(@RequestBody ArrayList<DokumentRed> dokumentRedove) {
+
+    }
+
 }
